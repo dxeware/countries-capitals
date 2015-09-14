a70d1e66564aad47c342d740f1f5a09b6ce657fa
dxeware <dxeware@gmail.com>
2015-09-14 10:13:28 -0700

    gulp build.....not working

--- gulpfile.js
+++ gulpfile.js
@@ -9,3 +9,19 @@ gulp.task('connect', function() {
   });
 });
 gulp.task('default', ['connect']);
+
+gulp.task('copy-html-files', function() {
+  gulp.src(['./app/**/*.html', './app/images/*', '!./app/index.html'], {base: './app'})
+  .pipe(gulp.dest('build/'));
+});
+
+gulp.task('usemin', function() {
+  gulp.src('./app/index.html')
+    .pipe(usemin({
+      css: [minifyCss(), 'concat', rev()],
+      js: [uglify(), rev()]
+    }))
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('build', ['copy-html-files', 'usemin']);
