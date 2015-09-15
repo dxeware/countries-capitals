4a460dda95b7ed05cbe3c429e5f7a44c88039f80
dxeware <dxeware@gmail.com>
2015-09-15 11:51:26 -0700

    Karma up and running...needed to add bower_components to 'exlude' section of karma.conf.js, needed to add *.js files in app/ directory

---
+++ app/countries/countries.Spec.js
@@ -0,0 +1,13 @@
+describe("ccAppViews", function() {
+    beforeEach(module('ccAppViews'));
+
+    describe('ccCountries', function() {
+      it('should return true when called', function() {
+            expect(true).toBe(true);
+      });
+      it('should return 10 when called', function() {
+            expect(10).toBe(10);
+      });
+    });
+});
+
