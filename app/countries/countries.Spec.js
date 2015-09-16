f5721537b5c5c74c505bd693026784eb941ecc61
dxeware <dxeware@gmail.com>
2015-09-16 12:39:46 -0700

    Got first real controller unit test working ... checked for variables defined and default values

--- app/countries/countries.Spec.js
+++ app/countries/countries.Spec.js
@@ -1,25 +1,28 @@
 describe("ccAppViews", function() {
 
-    var ctrl,
-      scope,
-      $location;
-
     beforeEach(module('ccAppViews'));
 
     // Initialize the controller and a mock scope
-    beforeEach(inject(function($controller, $rootScope, _$location_){
-      $location = _$location_;
-      scope = $rootScope.$new();
-      ctrl = $controller('CountriesCtrl', {
-        $scope: scope
-      });
-    }));
-    describe('ccCountries', function() {
-      it('should return true when called', function() {
-            expect(true).toBe(true);
-      });
-      it('should return 10 when called', function() {
-            expect(10).toBe(10);
+    describe('CountriesCtrl', function() {
+      var ctrl, scope;
+      beforeEach(inject(function($controller, $rootScope){
+        //$location = _$location_;
+        scope = $rootScope.$new();
+        ctrl = $controller('CountriesCtrl', {
+          $scope: scope
+        });
+      }));
+      // Check some defaults
+      it('should have some default variables defined', function(){
+        // Check if they are defined
+        expect(scope.pageClass).toBeDefined();
+        expect(scope.loading).toBeDefined();
+        expect(scope.dataReady).toBeDefined();
+        expect(scope.loading).toBeDefined();
+
+        // Check if the default values are correct
+        expect(scope.loading).toBeTruthy();
+        expect(scope.dataReady).toBeFalsy();
       });
     });
 
